a4e3afb6e2144943119dfa7e9cd678be69834384
Atul Kumar <atlpal1991@gmail.com>
2021-03-15 01:36:19 +0530

    integrating BrowserStack

--- XHelper/Helper.cs
+++ XHelper/Helper.cs
@@ -7,6 +7,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
 using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Remote;
 
 namespace ParallelTestsPOM.XHelper
 {
@@ -15,10 +16,22 @@ namespace ParallelTestsPOM.XHelper
 
         public static IWebDriver initilizer(int second)
         {
-            IWebDriver driver = new ChromeDriver();
+            OpenQA.Selenium.Chrome.ChromeOptions capability = new OpenQA.Selenium.Chrome.ChromeOptions();
+            capability.AddAdditionalCapability("os_version", "10", true);
+            capability.AddAdditionalCapability("resolution", "1920x1080", true);
+            capability.AddAdditionalCapability("browser", "Chrome", true);
+            capability.AddAdditionalCapability("browser_version", "latest-beta", true);
+            capability.AddAdditionalCapability("os", "Windows", true);
+            capability.AddAdditionalCapability("name", "BStack-[C_sharp] Sample Test", true); // test name
+            capability.AddAdditionalCapability("build", "BStack Build Number 1", true); // CI/CD job or build name
+            capability.AddAdditionalCapability("browserstack.user", "atulpal1", true);
+            capability.AddAdditionalCapability("browserstack.key", "qeHMaqH1ZVbRxbuLRoGs", true);
+            IWebDriver driver = new RemoteWebDriver(
+              new Uri("https://hub-cloud.browserstack.com/wd/hub/"), capability
+            );
             driver.Navigate().GoToUrl(Configuration.baseUrl);
 
-
+            //IWebDriver driver = new ChromeDriver(capability);
             TimeSpan seconds = TimeSpan.FromSeconds(second);
             driver.Manage().Timeouts().ImplicitWait = seconds;
 
